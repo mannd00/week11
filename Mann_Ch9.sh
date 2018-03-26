4cae8f279fa1ad1f3d523abe2020f5bbd69bb281
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-03-26 13:04:37 -0500

    Update Mann_Ch9.sh

--- Mann_Ch9.sh
+++ Mann_Ch9.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
 
+echo "Welcome to Chapter 9!"
+
+echo "First lets check our IP Address, along with some other network info with ifconfig"
+ifconfig
+
+route -n
+
+echo "Lets try pinging Google!"
+
+ping google.com -count 10
+
+echo "and we can check the route our packets take to get to Google"
+traceroute google.com
+
