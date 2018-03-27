a83e44cb25b5cc352f0b9592ebb944996b0f44f7
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-03-27 11:01:39 -0500

    Update Mann_Ch9.sh

--- Mann_Ch9.sh
+++ Mann_Ch9.sh
@@ -7,10 +7,25 @@ ifconfig
 
 route -n
 
-echo "Lets try pinging Google!"
+echo -e "\nWe can use ping on the loopback address 127.0.0.1 to verify our local Network card is up."
+ping -c 10 127.0.0.1
 
-ping google.com -count 10
+echo -e "\nNow, Lets try pinging Google!"
 
-echo "and we can check the route our packets take to get to Google"
+ping -c 10 google.com
+
+echo -e "\nand we can check the route our packets take to get to Google using traceroute."
 traceroute google.com
 
+echo -e "\nThe netstat command is very useful as it allows us to view current connections"
+netstat -nt
+
+echo -e "\nThe linux system has a powerful built in firewall system that can be configured using iptables. Lets see whats currently in there!"
+iptables -L
+
+echo -e "\nWe can use this to tell the system to drop any packets coming from a certain host, say 169.254.55.55 is a meany, lets block them!"
+iptables -A INPUT -s 169.254.55.55 -j DROP
+
+echo -e "\nOne last thing we can play with is scanning wireless networks (assuming we have a wireless NIC)"
+iw dev wlan0 scan
+iw dev wlan0 link
